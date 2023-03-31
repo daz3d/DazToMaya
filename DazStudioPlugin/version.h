f424cc20001e27a92347138d15bcd5b7a12fc7c9
danielbui78 <danielbui78@gmail.com>
2023-03-31 17:53:49 -0400

    version and package update

--- DazStudioPlugin/version.h
+++ DazStudioPlugin/version.h
@@ -4,7 +4,7 @@
 // Version number for dzBridge-Maya
 #define PLUGIN_MAJOR	2022
 #define PLUGIN_MINOR	2
-#define PLUGIN_REV		13
-#define PLUGIN_BUILD	37
+#define PLUGIN_REV		14
+#define PLUGIN_BUILD	38
 
 #define PLUGIN_VERSION	DZ_MAKE_VERSION( PLUGIN_MAJOR, PLUGIN_MINOR, PLUGIN_REV, PLUGIN_BUILD )
